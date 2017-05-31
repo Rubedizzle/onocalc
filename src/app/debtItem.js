2c3c13ce577287ee91b127bfe3c4a8432495774b
Rubedizzle <ruberry@gmail.com>
2017-05-31 19:45:37 -0400

    Added FA, and fa-icons to debts

--- src/app/debtItem.js
+++ src/app/debtItem.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class DebtItem extends React.Component{
+  constructor (){
+    super();
+    this.icons = {
+      creditCard: 'fa-credit-card',
+      car: 'fa-car',
+      creditLine: 'fa-university',
+      loan: 'fa-graduation-cap'
+    };
+  }
 
   render(){
+    var debtType = this.props.debt.debtType;
+    var debtIcon = 'fa-bar-chart';
+
+    if (debtType === 'Credit Card'){
+      debtIcon = this.icons.creditCard;
+    } else if (debtType === 'Car Loan'){
+      debtIcon = this.icons.car;
+    } else if (debtType === 'Line of Credit'){
+      debtIcon = this.icons.creditLine;
+    } else if (debtType === 'Student Loan'){
+      debtIcon = this.icons.loan;
+    }
+
     return (
         <div className="debt-item pure-u-1-3">
+          <i className={'fa ' + debtIcon + ' debtIcon'} aria-hidden="true"></i>
           <span className="debt-type">{this.props.debt.debtType}</span>
           <p>Total Debt: ${this.props.debt.totalDebt}</p>
           <p>Interest Rate: {this.props.debt.interestRate}%</p>
